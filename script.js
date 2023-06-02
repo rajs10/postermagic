608e913ba4a5531ec18e396bafa64d50d6e5d690
m-triple-m <triplem656@gmail.com>
2023-06-02 11:02:37 +0530

    ..

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const url = 'http://localhost:5000';
 
+
+
 const fetchDataFromBackend = async () => {
     const response = await fetch('http://localhost:3000');
+    var paramValue = getParameterByName('paramName');
+            if (paramValue) {
+                // Do something with the parameter value
+                console.log(paramValue);
+            }
     const data = await response.json();
     console.log(data);
 }
