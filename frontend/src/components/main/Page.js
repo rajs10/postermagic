cad6d165d4caac9a5b5ea40ae4d10f90a6201043
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-06-03 15:23:21 +0530

    ...

--- frontend/src/components/main/Page.js
+++ frontend/src/components/main/Page.js
@@ -1,109 +1,158 @@
-import React from 'react'
-import retail from './images/retail.gif'
-import education from './images/education.gif'
-import entertainment from './images/entertainment.gif'
-import tourism from './images/tourism.gif'
+import React from "react";
+import retail from "./images/retail.gif";
+import education from "./images/education.gif";
+import entertainment from "./images/entertainment.gif";
+import tourism from "./images/tourism.gif";
 
 const Page = () => {
-    return (
-        <div className=' mt-3'>
-            <div className="mt-3 text-center text-danger"><h2>
-                Our built-in solution
-            </h2>
-            </div>
-            <><div className='text-center'>
-                <>
-                    <a href='#Retail'
-                        type="button"
-                        className="btn btn-outline-danger btn-rounded m-2"
-                        data-mdb-ripple-color="dark"
-                    >
-                        Retail
-                    </a>
-                    <a href='#Education'
-                        type="button"
-                        className="btn btn-outline-danger btn-rounded m-2"
-                        data-mdb-ripple-color="dark"
-                    >
-                        Education
-                    </a>
-                    <a href='#Tourism'
-                        type="button"
-                        className="btn btn-outline-danger btn-rounded m-2"
-                        data-mdb-ripple-color="dark"
-                    >
-                        Tourism
-                    </a>
-                    <a href='#Entertainment'
-                        type="button"
-                        className="btn btn-outline-danger btn-rounded m-2"
-                        data-mdb-ripple-color="dark"
-                    >
-                        Entertainment
-                    </a>
-                </>
-                <hr className='mt-1 mb-1'></hr>
-            </div>
-            </>
-
-            <div className="d-flex">
-                <div className="p-2 w-100 float-md-left" id='Retail'><h3 className="card-title">Retail</h3>
-                    <p className="card-text">
-                        Enhance your retail experience with Postermagic AR! Step into a world where products come alive through augmented reality. Simply scan posters and watch as they transform into interactive displays, showcasing detailed information, product demos, and personalized recommendations. Visualize how furniture fits into your space, try on virtual clothing, or test out makeup virtually. With Postermagic AR, customers can make informed decisions, engage with brands in exciting ways, and enjoy a seamless blend of digital and physical shopping. Revolutionize your retail space and captivate your customers with this cutting-edge technology.
-
-                    </p>
-                </div>
-                <div className="p-2 flex-shrink-1" id='Retail'><img
-                    src={retail}
-                    className="w-100 shadow-1-strong rounded mb-4"
-                    alt="Retail"
-                />
-                </div>
-            </div>
-           
-            <div className="d-flex">
-                <div className="p-2 flex-shrink-1" id='Education'><img
-                    src={education}
-                    className="w-100 shadow-1-strong rounded mb-4"
-                    alt="Education"
-                />
-                </div>
-                <div className="p-2 w-100" id='Education'><h3 className="card-title">Education</h3>
-                    <p className="card-text">
-                        Elevate education with Postermagic AR! Transform traditional learning materials into immersive educational experiences. Students can scan posters to explore virtual worlds, dissect 3D models, and engage with interactive simulations. Dive into historical events, examine the human body, or explore scientific concepts in a whole new dimension. Postermagic AR brings subjects to life, making learning exciting and accessible for all. Teachers can create engaging lesson plans that captivate students' attention and enhance their understanding. With Postermagic AR, education becomes a journey of discovery and exploration, revolutionizing the classroom experience.
-
-                    </p>
-                </div>
-            </div>
-            <div className="d-flex">
-                <div className="p-2 w-100" id='Tourism'><h3 className="card-title">Tourism</h3>
-                    <p className="card-text">
-                        Experience a new dimension of tourism with Postermagic AR! Scan posters and embark on virtual adventures that transport you to iconic landmarks and breathtaking destinations. Explore ancient ruins, stroll through bustling cities, or relax on pristine beaches, all from the comfort of your surroundings. Postermagic AR provides interactive guides, historical insights, and immersive storytelling, making every trip a memorable and educational experience. Whether planning your next vacation or reliving past travels, let Postermagic AR ignite your wanderlust and bring the world to your fingertips. Discover the wonders of the world like never before!
-                    </p>
-                </div>
-                <div className="p-2 flex-shrink-1"><img
-                    src={tourism}
-                    className="w-100 shadow-1-strong rounded mb-4"
-                    alt="Tourism"
-                />
-                </div>
-            </div>
-            <div className="d-flex">
-                <div className="p-2 flex-shrink-1" id='Entertainment'><img
-                    src={entertainment}
-                    className="w-100 shadow-1-strong rounded mb-4"
-                    alt="Entertainment"
-                />
-                </div>
-                <div className="p-2 w-100"><h3 className="card-title">Entertainment</h3>
-                    <p className="card-text">
-                        Prepare for unparalleled entertainment with Postermagic AR! Immerse yourself in a world where posters come to life with captivating augmented reality experiences. Witness stunning 3D animations, meet virtual characters, and interact with engaging games. From thrilling action adventures to mesmerizing fantasy realms, Postermagic AR delivers an immersive entertainment revolution. Whether at home or in public spaces, unleash your imagination and let Postermagic AR transport you to new realms of fun and excitement. Get ready for a mind-bending fusion of reality and digital magic that will leave you spellbound.
-                    </p>
-                </div>
-            </div>
-            <hr className='mt-1 mb-1'></hr>
+  return (
+    <div className="mt-3">
+      <div className="mt-3 text-center text-danger">
+        <h2>Our built-in solution</h2>
+      </div>
+      <>
+        <div className="text-center">
+          <>
+            <a
+              href="#Retail"
+              type="button"
+              className="btn btn-outline-danger btn-rounded m-2"
+              data-mdb-ripple-color="dark"
+            >
+              Retail
+            </a>
+            <a
+              href="#Education"
+              type="button"
+              className="btn btn-outline-danger btn-rounded m-2"
+              data-mdb-ripple-color="dark"
+            >
+              Education
+            </a>
+            <a
+              href="#Tourism"
+              type="button"
+              className="btn btn-outline-danger btn-rounded m-2"
+              data-mdb-ripple-color="dark"
+            >
+              Tourism
+            </a>
+            <a
+              href="#Entertainment"
+              type="button"
+              className="btn btn-outline-danger btn-rounded m-2"
+              data-mdb-ripple-color="dark"
+            >
+              Entertainment
+            </a>
+          </>
+          <hr className="mt-1 mb-1"></hr>
         </div>
-    )
-}
+      </>
+      <div className="container">
+        <div className="d-flex">
+          <div className="p-2 w-100 float-md-left" id="Retail">
+            <h3 className="card-title">Retail</h3>
+            <p className="card-text">
+              Enhance your retail experience with Postermagic AR! Step into a
+              world where products come alive through augmented reality. Simply
+              scan posters and watch as they transform into interactive
+              displays, showcasing detailed information, product demos, and
+              personalized recommendations. Visualize how furniture fits into
+              your space, try on virtual clothing, or test out makeup virtually.
+              With Postermagic AR, customers can make informed decisions, engage
+              with brands in exciting ways, and enjoy a seamless blend of
+              digital and physical shopping. Revolutionize your retail space and
+              captivate your customers with this cutting-edge technology.
+            </p>
+          </div>
+          <div className="p-2 flex-shrink-1" id="Retail">
+            <img
+              src={retail}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Retail"
+            />
+          </div>
+        </div>
 
-export default Page
+        <div className="d-flex">
+          <div className="p-2 flex-shrink-1" id="Education">
+            <img
+              src={education}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Education"
+            />
+          </div>
+          <div className="p-2 w-100" id="Education">
+            <h3 className="card-title">Education</h3>
+            <p className="card-text">
+              Elevate education with Postermagic AR! Transform traditional
+              learning materials into immersive educational experiences.
+              Students can scan posters to explore virtual worlds, dissect 3D
+              models, and engage with interactive simulations. Dive into
+              historical events, examine the human body, or explore scientific
+              concepts in a whole new dimension. Postermagic AR brings subjects
+              to life, making learning exciting and accessible for all. Teachers
+              can create engaging lesson plans that captivate students'
+              attention and enhance their understanding. With Postermagic AR,
+              education becomes a journey of discovery and exploration,
+              revolutionizing the classroom experience.
+            </p>
+          </div>
+        </div>
+        <div className="d-flex">
+          <div className="p-2 w-100" id="Tourism">
+            <h3 className="card-title">Tourism</h3>
+            <p className="card-text">
+              Experience a new dimension of tourism with Postermagic AR! Scan
+              posters and embark on virtual adventures that transport you to
+              iconic landmarks and breathtaking destinations. Explore ancient
+              ruins, stroll through bustling cities, or relax on pristine
+              beaches, all from the comfort of your surroundings. Postermagic AR
+              provides interactive guides, historical insights, and immersive
+              storytelling, making every trip a memorable and educational
+              experience. Whether planning your next vacation or reliving past
+              travels, let Postermagic AR ignite your wanderlust and bring the
+              world to your fingertips. Discover the wonders of the world like
+              never before!
+            </p>
+          </div>
+          <div className="p-2 flex-shrink-1">
+            <img
+              src={tourism}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Tourism"
+            />
+          </div>
+        </div>
+        <div className="d-flex">
+          <div className="p-2 flex-shrink-1" id="Entertainment">
+            <img
+              src={entertainment}
+              className="w-100 shadow-1-strong rounded mb-4"
+              alt="Entertainment"
+            />
+          </div>
+          <div className="p-2 w-100">
+            <h3 className="card-title">Entertainment</h3>
+            <p className="card-text">
+              Prepare for unparalleled entertainment with Postermagic AR!
+              Immerse yourself in a world where posters come to life with
+              captivating augmented reality experiences. Witness stunning 3D
+              animations, meet virtual characters, and interact with engaging
+              games. From thrilling action adventures to mesmerizing fantasy
+              realms, Postermagic AR delivers an immersive entertainment
+              revolution. Whether at home or in public spaces, unleash your
+              imagination and let Postermagic AR transport you to new realms of
+              fun and excitement. Get ready for a mind-bending fusion of reality
+              and digital magic that will leave you spellbound.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="mt-1 mb-1"></hr>
+    </div>
+  );
+};
+
+export default Page;
