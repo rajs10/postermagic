3b930a89377466fa8d6579dd3a67ee19db8dd682
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-06-04 19:30:37 +0530

    working on feedback form

--- backend/Models/imageModel.js
+++ backend/Models/imageModel.js
@@ -1,9 +1,9 @@
-const { Types } = require('mongoose');
-const {Schema,model} = require('../connection');
+const { Types } = require("mongoose");
+const { Schema, model } = require("../connection");
 
 const myschema = new Schema({
-    image: String,
-    user: {type : Types.ObjectId, ref: 'users'},
-    createdAt: Date
+  image: String,
+  user: { type: Types.ObjectId, ref: "users" },
+  createdAt: Date,
 });
-module.exports = model('image',myschema);
+module.exports = model("image", myschema);
